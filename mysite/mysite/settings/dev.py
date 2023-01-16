efe931c8ed174f7af66ea007d9870931ed59100e
m-arrojo <113597466+m-arrojo@users.noreply.github.com>
2023-01-16 14:13:04 +0100

    creacion sitio mysite de wagtail y body

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-m2m#u2n_%o+@s!n+-(_qvpi6yk@ig*_6g#*z)ug3m^b(a-yq!("
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
